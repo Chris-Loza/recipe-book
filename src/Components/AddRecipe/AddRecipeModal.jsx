da9bfe6fb3f41797982b88ed15edea00b70d37d6
Christian Loza <christianloza@Christians-MacBook-Air.local>
2025-02-12 15:53:00 -0800

    added modal basic structure

--- src/Components/AddRecipe/AddRecipeModal.jsx
+++ src/Components/AddRecipe/AddRecipeModal.jsx
@@ -11,7 +11,13 @@ const AddRecipeModal = ({ showModal, setShowModal }) => {
     showModal && (
       <div className="modal" onClick={closeModal} ref={modalRef}>
         <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
-          Add Recipe Modal
+          <div className="modalHeader">Add a Recipe</div>
+          <div className="radioButtons">Time of day radio buttons</div>
+          <div className="recipe">Recipe</div>
+          <div className="nutritionSummary">Nutritional Summary</div>
+          <div className="recipeImgSubmit">
+            Recipe image and submit button
+          </div>
         </div>
       </div>
     )
