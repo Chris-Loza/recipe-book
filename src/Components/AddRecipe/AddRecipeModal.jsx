e1fb2cbdf325ae15a5a4feebae12f37b04f06e95
Christian Loza <christianloza@Christians-MacBook-Air.local>
2025-02-18 16:03:30 -0800

    added radio buttons in the add a recipe modal

--- src/Components/AddRecipe/AddRecipeModal.jsx
+++ src/Components/AddRecipe/AddRecipeModal.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./addRecipeModal.css";
 
 const AddRecipeModal = ({ showModal, setShowModal }) => {
@@ -7,17 +7,71 @@ const AddRecipeModal = ({ showModal, setShowModal }) => {
     setShowModal(false);
   };
 
+  const [selectedTime, setSelectedTime] = useState("Breakfast");
+
   return (
     showModal && (
       <div className="modal" onClick={closeModal} ref={modalRef}>
         <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
-          <div className="modalHeader">Add a Recipe</div>
-          <div className="radioButtons">Time of day radio buttons</div>
+          <div className="modalHeader">
+            <p>Add a Recipe</p>
+            <div className="botBorder"></div>
+          </div>
+          <div className="radioButtons">
+            <fieldset className="fieldSet">
+              <legend>Select a Time of Day</legend>
+              <label htmlFor="Breakfast">
+                <input
+                  type="radio"
+                  name="time"
+                  id="Breakfast"
+                  value={"Breakfast"}
+                  checked={selectedTime === "Breakfast"}
+                  onChange={(e) => setSelectedTime(e.target.value)}
+                />
+                <span>Breakfast</span>
+              </label>
+
+              <label htmlFor="Lunch">
+                <input
+                  type="radio"
+                  name="time"
+                  id="Lunch"
+                  value={"Lunch"}
+                  checked={selectedTime === "Lunch"}
+                  onChange={(e) => setSelectedTime(e.target.value)}
+                />
+                <span>Lunch</span>
+              </label>
+
+              <label htmlFor="Dinner">
+                <input
+                  type="radio"
+                  name="time"
+                  id="Dinner"
+                  value={"Dinner"}
+                  checked={selectedTime === "Dinner"}
+                  onChange={(e) => setSelectedTime(e.target.value)}
+                />
+                <span>Dinner</span>
+              </label>
+
+              <label htmlFor="Snacks">
+                <input
+                  type="radio"
+                  name="time"
+                  id="Snacks"
+                  value={"Snacks"}
+                  checked={selectedTime === "Snacks"}
+                  onChange={(e) => setSelectedTime(e.target.value)}
+                />
+                <span>Snacks</span>
+              </label>
+            </fieldset>
+          </div>
           <div className="recipe">Recipe</div>
           <div className="nutritionSummary">Nutritional Summary</div>
-          <div className="recipeImgSubmit">
-            Recipe image and submit button
-          </div>
+          <div className="recipeImgSubmit">Recipe image and submit button</div>
         </div>
       </div>
     )
