c79164edfbd1c31da8c2684425952d8565e26684
Christian Loza <christianloza@Christians-MacBook-Air.local>
2025-03-11 14:41:56 -0700

    worked on nutrition label

--- src/Components/RecipeCard/ExpandedCard.jsx
+++ src/Components/RecipeCard/ExpandedCard.jsx
@@ -19,53 +19,61 @@ const ExpandedCard = ({ cardExpand, setCardExpand }) => {
             </div>
             <div className="largeDivider"></div>
             <div className="calorieInfo">
-              <p>
-                <b>Amount per serving</b>
-              </p>
+              <div className="servingContainer">
+                <p>Amount per serving</p>
+                <p className="caloriesText">Calories</p>
+              </div>
               <div className="calories">
-                <p>Calories</p>
                 <p>230</p>
               </div>
             </div>
           </div>
           <div className="mediumDivider"></div>
           <div className="dailyValueText">
-            <p>
-              <b>% Daily Value *</b>
-            </p>
-            <div className="divider"></div>
-            <div className="fat">
-              <p>
-                <b>Total Fat</b> 9g
-              </p>
-              <p>
-                <b>10%</b>
-              </p>
+            <div className="dailyValuePercent">
+              <p>% Daily Value*</p>
             </div>
             <div className="divider"></div>
-            <div className="carbs">
-              <p>
-                <b>Total Carbohydrate</b> 37g
-              </p>
-              <p>
-                <b>10%</b>
-              </p>
+            <div className="macro">
+              <div className="totalMacro">
+                <p>
+                  <span>Total Fat</span> 9g
+                </p>
+              </div>
+              <div className="macroPercentage">
+                <p>10%</p>
+              </div>
             </div>
             <div className="divider"></div>
-            <div className="protein">
-              <p>
-                <b>Protein</b> 3g
-              </p>
-              <p>
-                <b>10%</b>
-              </p>
+            <div className="macro">
+              <div className="totalMacro">
+                <p>
+                  <span>Total Carbohydrate</span> 9g
+                </p>
+              </div>
+              <div className="macroPercentage">
+                <p>10%</p>
+              </div>
             </div>
+            <div className="divider"></div>
+            <div className="macro">
+              <div className="totalMacro">
+                <p>
+                  <span>Total Protein</span> 9g
+                </p>
+              </div>
+              <div className="macroPercentage">
+                <p>10%</p>
+              </div>
+            </div>
+            <div className="divider"></div>
+            {/* 
             <div className="largeDivider"></div>
             <p>
               The % Daily Value (DV) tells you how much a nutrient in a serving
               of food contributes to a daily diet. 2,000 calories a day is used
               for general nutrition advice.
-            </p>
+            </p> */}
           </div>
         </div>
         <div className="ingredientsList">Ingredients List</div>
