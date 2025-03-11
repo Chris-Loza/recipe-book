8f08bc34fe0a6cc89ddc897f3b081ae5872b316e
Christian Loza <christianloza@Christians-MacBook-Air.local>
2025-03-11 15:22:23 -0700

    finished nutrition label css

--- src/Components/RecipeCard/ExpandedCard.jsx
+++ src/Components/RecipeCard/ExpandedCard.jsx
@@ -66,14 +66,14 @@ const ExpandedCard = ({ cardExpand, setCardExpand }) => {
                 <p>10%</p>
               </div>
             </div>
-            <div className="divider"></div>
-            {/* 
             <div className="largeDivider"></div>
-            <p>
-              The % Daily Value (DV) tells you how much a nutrient in a serving
-              of food contributes to a daily diet. 2,000 calories a day is used
-              for general nutrition advice.
-            </p> */}
+            <div className="disclaimer">
+              <p>
+                The % Daily Value (DV) tells you how much a nutrient in a
+                serving of food contributes to a daily diet. 2,000 calories a
+                day is used for general nutrition advice.
+              </p>
+            </div>
           </div>
         </div>
         <div className="ingredientsList">Ingredients List</div>
