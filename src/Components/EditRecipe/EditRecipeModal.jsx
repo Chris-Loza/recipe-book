af8c5f532349b7d8d336634033feb5cb05968c57
Chris-Loza <christianloza99@gmail.com>
2025-03-20 14:38:15 -0700

    added a lot of edit modal functionality. Display newly editted information in modal and ingredient adding and removal from here. Need to implement old ingredient removal and final function that actually modifies appropriate recipe in appropriate timeOfDay recipe list

--- src/Components/EditRecipe/EditRecipeModal.jsx
+++ src/Components/EditRecipe/EditRecipeModal.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "./editRecipeModal.css";
 import { GlobalContext } from "../../Library/globalContext";
 
@@ -13,11 +13,116 @@ const EditRecipeModal = ({
 }) => {
   const { breakfastRecipes, lunchRecipes, dinnerRecipes, snacksRecipes } =
     useContext(GlobalContext);
-  const [editedTimeOfDay, setEditedTimeOfDay] = useState("");
+
+  const [displayImage, setDisplayImage] = useState(image);
+
+  const [editedTimeOfDay, setEditedTimeOfDay] = useState(
+    timeOfDay || "Breakfast"
+  );
+  const [editedTitle, setEditedTitle] = useState("");
   const [editedDescription, setEditedDescription] = useState("");
+  const [editedRecipeImage, setEditedRecipeImage] = useState({
+    file: null,
+    url: "",
+  });
+
+  const formRef = useRef(null);
+  const [addedIngredients, setAddedIngredients] = useState({
+    ingredientArray: [],
+  });
+  const [addedIngredientsTotalNutrition, setAddedIngredientsTotalNutrition] =
+    useState({
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+      quantity: 0,
+    });
+
+  useEffect(() => {
+    let totalAddedCalories = 0;
+    let totalAddedFat = 0;
+    let totalAddedCarbs = 0;
+    let totalAddedProtein = 0;
+    let totalAddedQuantity = 0;
+
+    addedIngredients.ingredientArray.forEach((ingredient) => {
+      totalAddedCalories += ingredient.calories;
+      totalAddedFat += ingredient.fat;
+      totalAddedCarbs += ingredient.carbs;
+      totalAddedProtein += ingredient.protein;
+      totalAddedQuantity += ingredient.quantity;
+    });
+
+    setAddedIngredientsTotalNutrition({
+      calories: totalAddedCalories,
+      fat: totalAddedFat,
+      carbs: totalAddedCarbs,
+      protein: totalAddedProtein,
+      quantity: totalAddedQuantity,
+    });
+  }, [addedIngredients]);
+
+  const handleIngredientAddition = (e) => {
+    e.preventDefault();
+
+    if (formRef.current) {
+      const ingredientData = new FormData(formRef.current);
+      const rawIngredientValues = Object.fromEntries(ingredientData.entries());
+
+      if (rawIngredientValues !== "") {
+        const ingredientValues = {
+          ingredient: rawIngredientValues.ingredient,
+          quantity: Number(rawIngredientValues.quantity),
+          calories: Number(rawIngredientValues.calories),
+          fat: Number(rawIngredientValues.fat),
+          carbs: Number(rawIngredientValues.carbs),
+          protein: Number(rawIngredientValues.protein),
+        };
+
+        setAddedIngredients((prev) => ({
+          ingredientArray: [...prev.ingredientArray, ingredientValues],
+        }));
+      }
+
+      formRef.current.reset();
+    }
+  };
+
+  const handleNewlyAddedIngredientRemoval = (index) => {
+    const updatedIngredientsArray = [...addedIngredients.ingredientArray];
+    updatedIngredientsArray.splice(index, 1);
+
+    setAddedIngredients({ ingredientArray: updatedIngredientsArray });
+  };
+
+  const handleImageEdit = (e) => {
+    if (e.target.files[0]) {
+      setEditedRecipeImage({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (editedRecipeImage.file !== null) {
+      setDisplayImage(editedRecipeImage.url);
+    }
+  }, [editedRecipeImage]);
+
+  const handleRecipeEdit = () => {
+    console.log("New Time of Day ", editedTimeOfDay);
+    console.log("New Description ", editedDescription);
+    console.log("New Title ", editedTitle);
+    console.log("New Iamge ", editedRecipeImage);
+    console.log("Newly Added Ingredients", addedIngredients);
+  };
+
   console.log(timeOfDay);
   console.log(dinnerRecipes[index]);
-  // Do this with the index of the recipe instead
+  console.log(image);
+  console.log(displayImage);
   return (
     showEditModal && (
       <div
@@ -38,7 +143,7 @@ const EditRecipeModal = ({
                   name="time"
                   id="Breakfast"
                   value={"Breakfast"}
-                  checked={timeOfDay === "Breakfast"}
+                  checked={editedTimeOfDay === "Breakfast"}
                   onChange={(e) => setEditedTimeOfDay(e.target.value)}
                 />
                 <span>Breakfast</span>
@@ -50,7 +155,7 @@ const EditRecipeModal = ({
                   name="time"
                   id="Lunch"
                   value={"Lunch"}
-                  checked={timeOfDay === "Lunch"}
+                  checked={editedTimeOfDay === "Lunch"}
                   onChange={(e) => setEditedTimeOfDay(e.target.value)}
                 />
                 <span>Lunch</span>
@@ -62,7 +167,7 @@ const EditRecipeModal = ({
                   name="time"
                   id="Dinner"
                   value={"Dinner"}
-                  checked={timeOfDay === "Dinner"}
+                  checked={editedTimeOfDay === "Dinner"}
                   onChange={(e) => setEditedTimeOfDay(e.target.value)}
                 />
                 <span>Dinner</span>
@@ -74,7 +179,7 @@ const EditRecipeModal = ({
                   name="time"
                   id="Snacks"
                   value={"Snacks"}
-                  checked={timeOfDay === "Snacks"}
+                  checked={editedTimeOfDay === "Snacks"}
                   onChange={(e) => setEditedTimeOfDay(e.target.value)}
                 />
                 <span>Snacks</span>
@@ -162,9 +267,27 @@ const EditRecipeModal = ({
                       )
                     )
                   : ""}
+                {addedIngredients.ingredientArray.length > 0 &&
+                  addedIngredients.ingredientArray.map((ingredient, index) => (
+                    <div className="ingredient" key={index}>
+                      <img
+                        src="../../../images/RemoveIconRed.svg"
+                        alt="Remove Icon"
+                        onClick={() => handleNewlyAddedIngredientRemoval(index)}
+                      />
+                      <p>
+                        {" "}
+                        Ingredient Name: {ingredient.ingredient} || Quantity:{" "}
+                        {ingredient.quantity}g || Calories:{" "}
+                        {ingredient.calories} cal || Fat: {ingredient.fat}g ||
+                        Carbs: {ingredient.carbs}g || Protein:{" "}
+                        {ingredient.protein}g
+                      </p>
+                    </div>
+                  ))}
               </div>
               <div className="ingredientInputs">
-                <form>
+                <form ref={formRef}>
                   <input
                     type="text"
                     name="ingredient"
@@ -202,7 +325,11 @@ const EditRecipeModal = ({
                     placeholder="Protein"
                   />
                 </form>
-                <img src="../../../images/AddBoxFill.svg" alt="Add Icon" />
+                <img
+                  src="../../../images/AddBoxFill.svg"
+                  alt="Add Icon"
+                  onClick={handleIngredientAddition}
+                />
               </div>
             </fieldset>
           </div>
@@ -212,32 +339,67 @@ const EditRecipeModal = ({
               <div className="totalMacros">
                 {timeOfDay === "Breakfast" ? (
                   <p>
-                    Total Calories: {breakfastRecipes[index].nutrition.calories}{" "}
-                    cal || Total Fat: {breakfastRecipes[index].nutrition.fat}g
-                    || Total Carbs: {breakfastRecipes[index].nutrition.carbs}g
-                    || Total Protein:{" "}
-                    {breakfastRecipes[index].nutrition.protein}g
+                    Total Calories:{" "}
+                    {breakfastRecipes[index].nutrition.calories +
+                      addedIngredientsTotalNutrition.calories}{" "}
+                    cal || Total Fat:{" "}
+                    {breakfastRecipes[index].nutrition.fat +
+                      addedIngredientsTotalNutrition.fat}
+                    g || Total Carbs:{" "}
+                    {breakfastRecipes[index].nutrition.carbs +
+                      addedIngredientsTotalNutrition.carbs}
+                    g || Total Protein:{" "}
+                    {breakfastRecipes[index].nutrition.protein +
+                      addedIngredientsTotalNutrition.protein}
+                    g
                   </p>
                 ) : timeOfDay === "Lunch" ? (
                   <p>
-                    Total Calories: {lunchRecipes[index].nutrition.calories} cal
-                    || Total Fat: {lunchRecipes[index].nutrition.fat}g || Total
-                    Carbs: {lunchRecipes[index].nutrition.carbs}g || Total
-                    Protein: {lunchRecipes[index].nutrition.protein}g
+                    Total Calories:{" "}
+                    {lunchRecipes[index].nutrition.calories +
+                      addedIngredientsTotalNutrition.calories}{" "}
+                    cal || Total Fat:{" "}
+                    {lunchRecipes[index].nutrition.fat +
+                      addedIngredientsTotalNutrition.fat}
+                    g || Total Carbs:{" "}
+                    {lunchRecipes[index].nutrition.carbs +
+                      addedIngredientsTotalNutrition.carbs}
+                    g || Total Protein:{" "}
+                    {lunchRecipes[index].nutrition.protein +
+                      addedIngredientsTotalNutrition.protein}
+                    g
                   </p>
                 ) : timeOfDay === "Dinner" ? (
                   <p>
-                    Total Calories: {dinnerRecipes[index].nutrition.calories}{" "}
-                    cal || Total Fat: {dinnerRecipes[index].nutrition.fat}g ||
-                    Total Carbs: {dinnerRecipes[index].nutrition.carbs}g ||
-                    Total Protein: {dinnerRecipes[index].nutrition.protein}g
+                    Total Calories:{" "}
+                    {dinnerRecipes[index].nutrition.calories +
+                      addedIngredientsTotalNutrition.calories}{" "}
+                    cal || Total Fat:{" "}
+                    {dinnerRecipes[index].nutrition.fat +
+                      addedIngredientsTotalNutrition.fat}
+                    g || Total Carbs:{" "}
+                    {dinnerRecipes[index].nutrition.carbs +
+                      addedIngredientsTotalNutrition.carbs}
+                    g || Total Protein:{" "}
+                    {dinnerRecipes[index].nutrition.protein +
+                      addedIngredientsTotalNutrition.protein}
+                    g
                   </p>
                 ) : timeOfDay === "Snacks" ? (
                   <p>
-                    Total Calories: {snacksRecipes[index].nutrition.calories}{" "}
-                    cal || Total Fat: {snacksRecipes[index].nutrition.fat}g ||
-                    Total Carbs: {snacksRecipes[index].nutrition.carbs}g ||
-                    Total Protein: {snacksRecipes[index].nutrition.protein}g
+                    Total Calories:{" "}
+                    {snacksRecipes[index].nutrition.calories +
+                      addedIngredientsTotalNutrition.calories}{" "}
+                    cal || Total Fat:{" "}
+                    {snacksRecipes[index].nutrition.fat +
+                      addedIngredientsTotalNutrition.fat}
+                    g || Total Carbs:{" "}
+                    {snacksRecipes[index].nutrition.carbs +
+                      addedIngredientsTotalNutrition.carbs}
+                    g || Total Protein:{" "}
+                    {snacksRecipes[index].nutrition.protein +
+                      addedIngredientsTotalNutrition.protein}
+                    g
                   </p>
                 ) : (
                   ""
@@ -257,20 +419,30 @@ const EditRecipeModal = ({
             <div className="editRecipeImageContainer">
               <label htmlFor="recipeImage">
                 <img
-                  src={image !== "" ? image : "../../../images/AddPhotoAlt.svg"}
+                  src={
+                    displayImage !== ""
+                      ? displayImage
+                      : "../../../images/AddPhotoAlt.svg"
+                  }
                   alt="Recipe Image"
                 />
                 Change Image
               </label>
-              <input style={{ display: "none" }} type="file" id="recipeImage" />
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="recipeImage"
+                onChange={handleImageEdit}
+              />
             </div>
             <div className="editRecipeTitle">
               <input
                 type="text"
                 placeholder={name !== "" ? name : "Edit Recipe Title"}
+                onChange={(e) => setEditedTitle(e.target.value)}
               />
             </div>
-            <div className="editButton">
+            <div className="editButton" onClick={handleRecipeEdit}>
               <p>Edit Recipe</p>
             </div>
           </div>
